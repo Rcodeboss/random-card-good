98854bd6711e133b698ceeaa5ab62666f9ce1ea2
Rcodeboss <morejonrafa@gmail.com>
2020-03-12 21:25:50 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,20 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let symbol = ["&#9824;"];
+  let number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let out = [""];
+  let s;
+  let n;
+
+  function randomC() {
+    for (s = 0; s < symbol.length; s++) {
+      for (n = 0; n < number.length; n++) {
+        out += symbol[s] + number[n] + "<br>";
+      }
+    }
+    document.getElementById("card").innerHTML = out;
+  }
+
+  console.log("rafa");
 };
