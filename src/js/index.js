14b90099ff5dc51b078bc0d05ed682ec3959e33b
Rcodeboss <morejonrafa@gmail.com>
2020-03-14 15:45:11 +0000

    javascript done

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,47 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  docuent;
+  const suite = ["♠", "♣", "♥", "♦"];
+  const number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const selectRandomIndex = array =>
+    array[Math.floor(Math.random() * (array.length - 1 - 0) + 0)];
+
+  const suiteToClass = suite => {
+    switch (suite) {
+      case "♠":
+        return "spade";
+      case "♣":
+        return "clubs";
+      case "♥":
+        return "hearts";
+      case "♦":
+        return "diamond";
+    }
+  };
+  const suiteE1 = document.querySelectorAll(".suite");
+  const numberE1 = document.querySelector(".number");
+
+  const suiteValue = selectRandomIndex(suite);
+  const numberValue = selectRandomIndex(number);
+  console.log(suiteValue, numberValue);
+  console.log(suiteE1);
+  suiteE1.forEach(e1 => {
+    e1.classList.add(suiteToClass(suiteValue));
+    e1.textContent = suiteValue;
+  });
+  numberE1.textContent = numberValue;
 };
